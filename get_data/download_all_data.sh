a541b55fbfa59cf5fd82acce517868c1866a55e2
Philip Brohan <philip@brohan.org>
2024-06-12 12:01:29 +0100

    Initialize docs branch

--- get_data/download_all_data.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-# Run all the data downloads
-
-echo -n 'HadCRUT'
-(cd HadCRUT && ./get_members.py)
-
--- make_raw_tensors/make_all_raw_tensors.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-# Make all the raw tensors
-# Requires downloaded data
-
-(cd HadCRUT && ./make_all_tensors.py)
